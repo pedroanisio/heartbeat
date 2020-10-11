3c35ca5735b427fe3a64383e22590abd21a1b19f
Pedro Anisio Silva <pedroanisio@gmail.com>
2020-10-11 18:23:58 -0300

    latest

--- hbeat.py
+++ hbeat.py
@@ -1,10 +1,16 @@
 import time
 import os
+from pathlib import Path
 
-# os.chdir("/home/udi/foo")
+home_path: str = str(Path.home())
+home_path = f"{home_path}/cron"
 
+if not os.path.exists(home_path):
+    os.makedirs(home_path)
+
+os.chdir(home_path)
 beats = "beats.txt"
 t = time.time()
 log = open(beats, "a")
-log.write(str(t)+"\n")
+log.write(str(t) + "\n")
 log.close()
